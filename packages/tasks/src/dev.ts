6b2ca9c41bf583320a2dbc26c58f210336e15d6c
rondymesquita <rondinellimesquitas@gmail.com>
2023-01-10 08:59:28 -0300

    feat: namespaces

--- packages/tasks/src/dev.ts
+++ packages/tasks/src/dev.ts
@@ -1,31 +1,33 @@
-import { TaskContext, tasks } from './index'
+import { TaskContext, tasks, namespace, help } from './index'
 
+help(build, 'build app')
 function build(ctx: TaskContext) {
   console.log('building', ctx)
 }
 
-const clean = async (ctx: TaskContext) => {
+help(clean, 'clean app')
+async function clean(ctx: TaskContext) {
   console.log('cleaning', ctx)
   await new Promise((res) => setTimeout(res, 2000))
 }
 
-const test: any = {
-  watch: (ctx: TaskContext) => {
-    console.log('test:watch')
-  },
-  default: (ctx: TaskContext) => {
-    console.log('test')
-  },
-  cov: {
-    watch: (ctx: TaskContext) => {
-      console.log('test:cov:watch')
-    },
-    default: [clean, build],
-  },
-}
+const test = namespace('test', ({ help, tasks }) => {
+  help(build, 'build app')
+  function build(ctx: TaskContext) {
+    console.log('building', ctx)
+  }
+
+  help(clean, 'clean app')
+  async function clean(ctx: TaskContext) {
+    console.log('cleaning', ctx)
+    await new Promise((res) => setTimeout(res, 2000))
+  }
+
+  return tasks({ clean, build })
+})
 
 tasks({
   clean,
   build: [clean, build],
-  test,
+  ...test,
 })
--- packages/tasks/src/index.ts
+++ packages/tasks/src/index.ts
@@ -55,15 +55,60 @@ function deepFlattenObject(obj: any, namespace = '', tasks: any = {}) {
   return tasks
 }
 
-const createTasks = (
-  taskDef: TaskDef | Task | Task[],
-  tasks: any = {},
-  namespace: string = '',
-) => {
-  return deepFlattenObject(taskDef)
+// const createTasks = (
+//   taskDef: TaskDef | Task | Task[],
+//   tasks: any = {},
+//   namespace: string = '',
+// ) => {
+//   return deepFlattenObject(taskDef)
+// }
+
+const helpMessages: { [key: string]: string } = {}
+
+export const help = (task: Task, description: string) => {
+  helpMessages[task.name] = description
 }
 
-export const tasks = async (taskDef: TaskDef) => {
+const createHelpFunction = (namespace: string) => {
+  // const name = namespace
+  const help = (task: Task, description: string) => {
+    const taskName = buildTaskName(namespace, task.name)
+    helpMessages[taskName] = description
+  }
+
+  return help
+}
+
+function createTasksFunction(namespace: string = '') {
+  const name = namespace
+  const createTasks = (
+    taskDef: TaskDef | Task | Task[],
+    tasks: any = {},
+    namespace: string = '',
+  ) => {
+    return deepFlattenObject(taskDef, name)
+  }
+  return createTasks
+}
+
+export type CreateNamespaceFn = (...params: any[]) => Promise<any> | any
+
+export const namespace = (
+  name: string,
+  createNamespaceFn: CreateNamespaceFn,
+): TaskDef => {
+  // console.log({ name })
+
+  const namespacedHelp = createHelpFunction(name)
+
+  const createNamespacedTasks = createTasksFunction(name)
+  return createNamespaceFn({
+    help: namespacedHelp,
+    tasks: createNamespacedTasks,
+  })
+}
+
+export async function tasks(taskDef: TaskDef) {
   const argv = args(process.argv.slice(2))
   const name = argv.params[0]
 
@@ -71,27 +116,29 @@ export const tasks = async (taskDef: TaskDef) => {
     argv,
   }
 
+  const createTasks = createTasksFunction()
   const tasks = createTasks(taskDef)
+  console.log(tasks)
 
-  const task = name ? tasks[name] : tasks.default
+  // const task = name ? tasks[name] : tasks.default
 
-  const isThereAnyNonDefaultTask =
-    Object.keys(tasks).filter((name: string) => name !== 'default').length > 0
+  // const isThereAnyNonDefaultTask =
+  //   Object.keys(tasks).filter((name: string) => name !== 'default').length > 0
 
-  if (!name && isThereAnyNonDefaultTask) {
-    throw new TaskNameNotInformedError()
-  }
+  // if (!name && isThereAnyNonDefaultTask) {
+  //   throw new TaskNameNotInformedError()
+  // }
 
-  if (!task) {
-    throw new TaskNotFoundError(name)
-  }
+  // if (!task) {
+  //   throw new TaskNotFoundError(name)
+  // }
 
-  if (Array.isArray(task)) {
-    for (const step of task) {
-      await step(ctx)
-    }
-    return
-  }
+  // if (Array.isArray(task)) {
+  //   for (const step of task) {
+  //     await step(ctx)
+  //   }
+  //   return
+  // }
 
-  await task(ctx)
+  // await task(ctx)
 }
