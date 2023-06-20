cf6de3c20f80d4376b08930539e38339895f7fa4
rondymesquita <rondinellimesquitas@gmail.com>
2023-06-20 10:04:34 -0300

    chore: export service

--- packages/nutshell/src/globals.ts
+++ packages/nutshell/src/globals.ts
@@ -1,12 +1,14 @@
 import * as models from './models'
 import * as libs from './libs'
 import * as core from './core'
+import * as service from './service'
 import * as tasks from '@rondymesquita/tasks'
 import * as args from '@rondymesquita/args'
 
 Object.assign(global, models)
 Object.assign(global, libs)
 Object.assign(global, core)
+Object.assign(global, service)
 Object.assign(global, tasks)
 Object.assign(global, args)
 
